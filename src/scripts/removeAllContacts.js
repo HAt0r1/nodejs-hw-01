315cc1aa81eb07be0df2740e81300490fbe9c56a
Hatori <ahgangadm@gmail.com>
2024-06-08 16:12:03 +0300

    Update removeAllContacts function

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    contacts.length = 0;
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts));
+    console.log('Operation dalete was success');
+  } catch (error) {
+    console.log(`Error in delete. Error: ${error}`);
+  }
+};
 
 await removeAllContacts();
