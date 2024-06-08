da742a773907456608cc50984f050335ee78f1ae
Hatori <ahgangadm@gmail.com>
2024-06-08 16:08:55 +0300

    Update generateContacts function

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    for (let i = 0; i <= number; i++) {
+      contacts.push(createFakeContact());
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts));
+    console.log('Operation success');
+  } catch (error) {
+    console.log(`Operation failed. Error: ${error}`);
+  }
+};
 
 await generateContacts(5);
