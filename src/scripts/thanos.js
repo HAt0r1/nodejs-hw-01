50b0e94ff26b01739f4348de2f9246c4d9b2cb5b
Hatori <ahgangadm@gmail.com>
2024-06-08 16:14:53 +0300

    Update Thanos function

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,23 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    const thanosArray = [];
+
+    contacts.forEach((contact) => {
+      if (Math.random() >= 0.5) {
+        thanosArray.push(contact);
+      }
+    });
+
+    await fs.writeFile(PATH_DB, JSON.stringify(thanosArray));
+    console.log('Thanos delete some element in your array');
+  } catch (error) {
+    console.log(`Thanos misclick. Error: ${error}`);
+  }
+};
 
 await thanos();
